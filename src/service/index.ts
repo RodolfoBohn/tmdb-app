01218ce0e54d6cb0c8731fe62c3bf68cdce0a674
RodolfoBohn <ensino.rodolfofbohn@gmail.com>
2022-01-29 17:06:24 -0300

    Ajustando idioma da service

--- src/service/index.ts
+++ src/service/index.ts
@@ -5,22 +5,22 @@ import { CastResponse, MovieDetailsResponse, MovieListResponse } from '../@types
 class Service {
 
   async getMovieDetails(id: string) {
-    const response = await this.getAxiosInstance().get(`/movie/${id}?api_key=${process.env.TMDB_API_KEY}&language=pt-BR`)
+    const response = await this.getAxiosInstance().get(`/movie/${id}?api_key=${process.env.TMDB_API_KEY}&language=pt-br`)
     return response.data as MovieDetailsResponse
   }
 
   async getTrendingMoviesPerDay() {
-    const response = await this.getAxiosInstance().get(`/trending/movie/day?api_key=${process.env.TMDB_API_KEY}&language=pt-BR`)
+    const response = await this.getAxiosInstance().get(`/trending/movie/day?api_key=${process.env.TMDB_API_KEY}&language=pt-br`)
     return response.data.results as CastResponse[]
   }
 
   async getTrendingMoviesPerWeek() {
-    const response = await this.getAxiosInstance().get(`/trending/movie/week?api_key=${process.env.TMDB_API_KEY}&language=pt-BR`)
+    const response = await this.getAxiosInstance().get(`/trending/movie/week?api_key=${process.env.TMDB_API_KEY}&language=pt-br`)
     return response.data.results as CastResponse[]
   }
 
   async getMovieCast(id:string) {
-    const response = await this.getAxiosInstance().get(`/movie/${id}/credits?api_key=${process.env.TMDB_API_KEY}&language=pt-BR`)
+    const response = await this.getAxiosInstance().get(`/movie/${id}/credits?api_key=${process.env.TMDB_API_KEY}&language=pt-br`)
     return response.data.cast as CastResponse[]
   }
   
